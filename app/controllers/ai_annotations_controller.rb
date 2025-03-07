57318f9afde28fe8d8e9fde1d6e614e3184e4b3d
yush-nh <yushin.nihei@luxiar.com>
2025-03-07 16:52:58 +0900

    Set ai annotation content to instance variable

--- app/controllers/ai_annotations_controller.rb
+++ app/controllers/ai_annotations_controller.rb
@@ -1,5 +1,5 @@
 class AiAnnotationsController < ApplicationController
   def show
-    @ai_annotation = AiAnnotation.find_by!(uuid: params[:uuid])
+    @ai_annotation = AiAnnotation.find_by!(uuid: params[:uuid]).content
   end
 end

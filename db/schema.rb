5311a196bae82a221f119ad43e22e1b9f06f13a7
yush-nh <yushin.nihei@luxiar.com>
2025-03-07 11:21:45 +0900

    Created AiAnnotation table

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_07_021604) do
+  create_table "ai_annotations", force: :cascade do |t|
+    t.string "uuid", null: false
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end

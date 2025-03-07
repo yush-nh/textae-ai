d2c57de513683de88c87785673b3f34594c81b2f
yush-nh <yushin.nihei@luxiar.com>
2025-03-07 16:51:39 +0900

    Fix SQL to find ai annotation

--- app/controllers/ai_annotations_controller.rb
+++ app/controllers/ai_annotations_controller.rb
@@ -1,5 +1,5 @@
 class AiAnnotationsController < ApplicationController
   def show
-    @ai_annotation = AiAnnotation.find_by!(uuid: params[:id])
+    @ai_annotation = AiAnnotation.find_by!(uuid: params[:uuid])
   end
 end

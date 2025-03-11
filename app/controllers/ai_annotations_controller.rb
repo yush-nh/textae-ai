efe35e6bf7199af9f15e0a53bfb3f132cd5eb8a7
yush-nh <yushin.nihei@luxiar.com>
2025-03-11 18:44:40 +0900

    Move annotation conversion logic to class

--- app/controllers/ai_annotations_controller.rb
+++ app/controllers/ai_annotations_controller.rb
@@ -6,7 +6,7 @@ class AiAnnotationsController < ApplicationController
   def new; end
 
   def create
-    text = params[:text] || textae_annotation
+    text = params[:text] || AnnotationConverter.to_inline(params[:textae_annotation])
     prompt = params[:prompt]
     ai_annotation = AiAnnotation.generate!(text, prompt)
 
@@ -21,19 +21,5 @@ class AiAnnotationsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
-  end
-
-  private
-
-  def textae_annotation
-    uri = URI.parse("https://pubannotation.org/conversions/json2inline")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-
-    request = Net::HTTP::Post.new(uri.request_uri)
-    request["Content-Type"] = "application/json"
-    request.body = params[:textae_annotation]
-
-    http.request(request).body.force_encoding('UTF-8')
   end
 end
---
+++ app/models/AnnotationConverter.rb
@@ -0,0 +1,13 @@
+class AnnotationConverter
+  def self.to_inline(json)
+    uri = URI.parse("https://pubannotation.org/conversions/json2inline")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request["Content-Type"] = "application/json"
+    request.body = json
+
+    http.request(request).body.force_encoding('UTF-8')
+  end
+end

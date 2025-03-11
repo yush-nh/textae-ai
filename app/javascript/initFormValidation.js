c710d31a1bf696ed244cd980eb8737b244346744
yush-nh <yushin.nihei@luxiar.com>
2025-03-11 16:59:04 +0900

    Refactor initFormValidation

--- app/javascript/initFormValidation.js
+++ app/javascript/initFormValidation.js
@@ -2,15 +2,15 @@ const initFormValidation = () => {
   const form = document.querySelector(".ai-annotation-form")
   if (!form) return
 
-  const commit = form.elements.commit
+  const { text, prompt, commit } = form.elements
   const enableSubmitButton = () => commit.disabled = !form.checkValidity()
 
-  if (form.elements.text) {
+  if (text) {
     form.elements.text.addEventListener("input", enableSubmitButton)
   }
 
-  if (form.elements.prompt) {
-    form.elements.prompt.addEventListener("input", enableSubmitButton)
+  if (prompt) {
+    prompt.addEventListener("input", enableSubmitButton)
   }
 }
 

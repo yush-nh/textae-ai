98884fb45e4e4d508b7429f2010b8f5021751558
yush-nh <yushin.nihei@luxiar.com>
2025-03-12 11:07:08 +0900

    Refactor AnnotationConverter

--- app/models/annotation_converter.rb
+++ app/models/annotation_converter.rb
@@ -1,14 +1,27 @@
 class AnnotationConverter
   @uri = URI.parse("https://pubannotation.org/conversions/json2inline")
 
-  def self.to_inline(json)
-    http = Net::HTTP.new(@uri.host, @uri.port)
-    http.use_ssl = true
+  class << self
+    def to_inline(json)
+      http = prepare_http
+      request = prepare_request
+      request.body = json
 
-    request = Net::HTTP::Post.new(@uri.request_uri)
-    request["Content-Type"] = "application/json"
-    request.body = json
+      http.request(request).body.force_encoding("UTF-8")
+    end
 
-    http.request(request).body.force_encoding("UTF-8")
+    private
+
+    def prepare_http
+      http = Net::HTTP.new(@uri.host, @uri.port)
+      http.use_ssl = true
+      http
+    end
+
+    def prepare_request
+      request = Net::HTTP::Post.new(@uri.request_uri)
+      request["Content-Type"] = "application/json"
+      request
+    end
   end
 end
